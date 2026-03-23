c3ea7d7123cdac3aabe2766d809ed64e7bafcd26
DEVIKA975 <devika_aj@outlook.com>
2026-03-23 09:37:44 +0100

    Final commit session for recently visited movies

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -6,14 +6,22 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login/')
 def movie_list(request):
-    print(request.COOKIES)
-    visits = request.COOKIES.get('visits', 0)
-    visits = int(visits) + 1
+    session_count = request.session.get('count',0)
+    session_count =int(session_count) + 1
+    request.session['count'] = session_count
+
+    #print(request.COOKIES)
+    cookie_visits = request.COOKIES.get('visits', 0)
+    visits_from_cookie = int(cookie_visits) + 1
     movie_data = MovieInfo.objects.all()
     #print(movie_data)
 
-    response = render(request,'list.html',{'movies': movie_data, 'visits': visits} )
-    response.set_cookie('visits', visits)
+
+    recent_visits = request.session.get('recent_visits', [])
+    recent_movies = MovieInfo.objects.filter(pk__in=recent_visits)
+
+    response = render(request,'list.html',{'movies': movie_data, 'visits_from_cookie': visits_from_cookie, 'visits_from_session': session_count, 'recent_movies': recent_movies} )
+    response.set_cookie('visits',   visits_from_cookie)
     return response
 
 @login_required(login_url='login/')
@@ -42,6 +50,9 @@ def movie_edit(request, pk):
         if frm.is_valid():
             edit_movie.save()
     else:   
+        recent_visits = request.session.get('recent_visits', [])
+        recent_visits.insert(0,pk)
+        request.session['recent_visits'] = recent_visits[:5]  # Keep only the last 5 visited movies
         frm = MovieInfoForm(instance=edit_movie)
 
     return render(request,'create.html', {'frm': frm})
